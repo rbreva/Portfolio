f529fb31a6a0c7e1f9d726e76a3b991325044eac
Mauricio Mendoza <rbreva@gmail.com>
2022-09-07 19:33:11 -0500

    Added functions to open and close modal, added blur to the background, added cards dynamically

--- js/project-data.js
+++ js/project-data.js
@@ -1,17 +1,9 @@
 const projectData = [
   {
-    name: 'Multi-Post Stories',
-    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.',
-    technologies: ['css', 'html', 'bootstrap', 'Ruby'],
-    image: 'image.jpg',
-    liveVersion: 'liveVersion',
-    linkSource: 'linkSource',
-  },
-  {
     name: 'Profesional Art Printing Data More',
     description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard',
     technologies: ['html', 'bootstrap', 'Ruby'],
-    image: 'image.jpg',
+    image: 'card_portfolio01.jpg',
     liveVersion: 'liveVersion',
     linkSource: 'linkSource',
   },
@@ -19,7 +11,7 @@ const projectData = [
     name: 'Data Dashboard Healthcare',
     description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard',
     technologies: ['html', 'bootstrap', 'Ruby'],
-    image: 'image.jpg',
+    image: 'card_portfolio02.jpg',
     liveVersion: 'liveVersion',
     linkSource: 'linkSource',
   },
@@ -27,15 +19,7 @@ const projectData = [
     name: 'Website Portfolio',
     description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.',
     technologies: ['css', 'html', 'bootstrap', 'Ruby'],
-    image: 'image.jpg',
-    liveVersion: 'liveVersion',
-    linkSource: 'linkSource',
-  },
-  {
-    name: 'Multi-Post Stories',
-    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.',
-    technologies: ['css', 'html', 'bootstrap', 'Ruby'],
-    image: 'image.jpg',
+    image: 'card_portfolio03.jpg',
     liveVersion: 'liveVersion',
     linkSource: 'linkSource',
   },
@@ -43,18 +27,35 @@ const projectData = [
     name: 'Profesional Art Printing Data More',
     description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard',
     technologies: ['html', 'bootstrap', 'Ruby'],
-    image: 'image.jpg',
+    image: 'card_portfolio01.jpg',
+    liveVersion: 'liveVersion',
+    linkSource: 'linkSource',
+  },
+  {
+    name: 'Data Dashboard Healthcare',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard',
+    technologies: ['html', 'bootstrap', 'Ruby'],
+    image: 'card_portfolio02.jpg',
+    liveVersion: 'liveVersion',
+    linkSource: 'linkSource',
+  },
+  {
+    name: 'Website Portfolio',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.',
+    technologies: ['css', 'html', 'bootstrap', 'Ruby'],
+    image: 'card_portfolio03.jpg',
     liveVersion: 'liveVersion',
     linkSource: 'linkSource',
   },
 ];
 
-// console.log(projectData);
-
-// const newElement = document.createElement("h3");
-// newElement.classList.add("card__h3");
-// newElement.textContent = "Nombre: "+projectData[3].name;
-// document.querySelector(".cards").appendChild(newElement);
+const popModal = document.querySelector('.popup');
+const hero = document.querySelector('.hero__container');
+const aboutme = document.querySelector('.aboutme');
+const contact = document.querySelector('.form__content');
+const works = document.querySelector('.works');
+const footer = document.querySelector('.footer');
+const modalClose = document.querySelector('#closeModal');
 
 for (let i = 0; i < projectData.length; i += 1) {
 // console.log(projectData[i]);
@@ -88,6 +89,7 @@ for (let i = 0; i < projectData.length; i += 1) {
   const cardBtn = document.createElement('button');
   cardBtn.type = 'button';
   cardBtn.classList.add('card__btn');
+  cardBtn.setAttribute('onclick', `openPopup(${i})`);
   cardBtn.textContent = 'See Project';
 
   cardContainer.appendChild(cardH3);
@@ -99,3 +101,27 @@ for (let i = 0; i < projectData.length; i += 1) {
 
   document.querySelector('.cards').appendChild(cardsCard);
 }
+
+function openPopup(i) {
+  popModal.classList.remove('hidden');
+
+  document.querySelector('.popup__header__title').innerHTML = projectData[i].name;
+  document.querySelector('.popup__p').innerHTML = projectData[i].description;
+  document.querySelector('.popup__image').src = `/images/cards/${projectData[i].image}`;
+
+  hero.classList.add('blur');
+  aboutme.classList.add('blur');
+  contact.classList.add('blur');
+  works.classList.add('blur');
+  footer.classList.add('blur');
+}
+
+modalClose.addEventListener('click', () => {
+  popModal.classList.add('hidden');
+
+  hero.classList.remove('blur');
+  aboutme.classList.remove('blur');
+  contact.classList.remove('blur');
+  works.classList.remove('blur');
+  footer.classList.remove('blur');
+});
