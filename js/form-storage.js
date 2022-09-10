2c0c32182a74ea80fe855a8316bc5a1bed118513
Mauricio Mendoza <rbreva@gmail.com>
2022-09-10 10:52:31 -0500

    Fixed linter errors

--- js/form-storage.js
+++ js/form-storage.js
@@ -1,13 +1,22 @@
-
 const formData = localStorage.getItem('formData');
 const fullnamels = document.getElementById('fullname');
 const emailaddressls = document.getElementById('emailaddress');
 const textareaFormls = document.getElementById('textareaForm');
 let dataForm = {
-    name: '',
-    email: '',
-    message: '',
-  };
+  name: '',
+  email: '',
+  message: '',
+};
+
+function addToLocal() {
+  dataForm.name = fullnamels.value;
+  dataForm.email = emailaddressls.value;
+  dataForm.message = textareaFormls.value;
+
+  const data = JSON.stringify(dataForm);
+
+  localStorage.setItem('formData', data);
+}
 
 fullnamels.addEventListener('change', () => {
   addToLocal();
@@ -21,22 +30,11 @@ textareaFormls.addEventListener('change', () => {
   addToLocal();
 });
 
-function addToLocal() {
-  dataForm.name = fullnamels.value;
-  dataForm.email = emailaddressls.value;
-  dataForm.message = textareaFormls.value;
-
-  const data = JSON.stringify(dataForm);
-
-  localStorage.setItem('formData', data);
-}
-
 if (formData === null) {
-  console.log('Empty element');
-}else {
+  // console.log('Empty element');
+} else {
   dataForm = JSON.parse(formData);
 
-  //console.log(dataForm);
   document.getElementById('fullname').value = dataForm.name;
   document.getElementById('emailaddress').value = dataForm.email;
   document.getElementById('textareaForm').value = dataForm.message;
