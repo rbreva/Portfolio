e05e71cfa0336b907f7d32bbe177105dbfdb6fa6
Osvaldo Barrios Cortina <osvaldo.barrios@gmail.com>
2022-09-10 10:26:15 -0500

    Add pre-filled data in the input field form and add style linters

--- js/form-storage.js
+++ js/form-storage.js
@@ -6,28 +6,38 @@ const textareaFormls = document.getElementById('textareaForm');
 let dataForm = {
     name: '',
     email: '',
-    message: ''
+    message: '',
   };
 
 fullnamels.addEventListener('change', () => {
-  addToLocal ();
-})
+  addToLocal();
+});
 
 emailaddressls.addEventListener('change', () => {
-  addToLocal ();
-})
+  addToLocal();
+});
 
 textareaFormls.addEventListener('change', () => {
-  addToLocal ();
-})
+  addToLocal();
+});
 
-function addToLocal (){
-  dataForm['name'] = fullnamels.value;
-  dataForm['email'] = emailaddressls.value;
-  dataForm['message'] = textareaFormls.value;
+function addToLocal() {
+  dataForm.name = fullnamels.value;
+  dataForm.email = emailaddressls.value;
+  dataForm.message = textareaFormls.value;
 
   const data = JSON.stringify(dataForm);
-  //console.log(data);
+
   localStorage.setItem('formData', data);
 }
 
+if (formData === null) {
+  console.log('Empty element');
+}else {
+  dataForm = JSON.parse(formData);
+
+  //console.log(dataForm);
+  document.getElementById('fullname').value = dataForm.name;
+  document.getElementById('emailaddress').value = dataForm.email;
+  document.getElementById('textareaForm').value = dataForm.message;
+}
